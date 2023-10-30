260ab399e8e88c341db26c2f99067def90c9dbff
JellyfishKnight <1002984726@qq.com>
2023-10-30 17:21:15 +0800

    start detector node

---
+++ detector_node/src/DetectorNode.cpp
@@ -0,0 +1,21 @@
+// created by liuhan on 2023/10/29
+// Submodule of HeliosRobotSystem
+// for more see document: https://swjtuhelios.feishu.cn/docx/MfCsdfRxkoYk3oxWaazcfUpTnih?from=from_copylink
+/*
+ * ██   ██ ███████ ██      ██  ██████  ███████
+ * ██   ██ ██      ██      ██ ██    ██ ██
+ * ███████ █████   ██      ██ ██    ██ ███████
+ * ██   ██ ██      ██      ██ ██    ██      ██
+ * ██   ██ ███████ ███████ ██  ██████  ███████
+ */
+
+#include "DetectorNode.hpp"
+
+namespace helios_cv {
+
+} // namespace helios_cv
+
+// register node to component
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(helios_cv::DetectorNode);
