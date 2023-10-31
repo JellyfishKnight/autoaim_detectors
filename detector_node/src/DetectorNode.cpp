b8e293556995aad57493dafd6e7b2787d6131a31
JellyfishKnight <1002984726@qq.com>
2023-10-31 22:24:43 +0800

    detector node build failed

--- detector_node/src/DetectorNode.cpp
+++ detector_node/src/DetectorNode.cpp
@@ -13,6 +13,19 @@
 
 namespace helios_cv {
     
+DetectorNode::DetectorNode(const rclcpp::NodeOptions& options) : rclcpp::Node("detector_node", options) {
+    try {
+        param_listener_ = std::make_shared<gimbal_controller::ParamListener>(get_node());
+        params_ = param_listener_->get_params();
+    } catch (const std::exception &e) {
+        RCLCPP_FATAL(logger_, "Failed to get parameters: %s, use empty params", e.what());
+    }
+    
+}
+
+~DetectorNode::DetectorNode() {
+
+}
 
 } // namespace helios_cv
 
