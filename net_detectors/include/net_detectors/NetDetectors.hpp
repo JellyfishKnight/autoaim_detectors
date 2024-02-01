4db20269adf6ff7a303634e2dfc39aefab426299
jellyfishknight <1002984726@qq.com>
2024-02-01 20:58:00 +0800

    finished ov net detector

--- net_detectors/include/net_detectors/NetDetectors.hpp
+++ net_detectors/include/net_detectors/NetDetectors.hpp
@@ -11,22 +11,40 @@
 #pragma once
 
 #include "BaseNetDetector.hpp"
+#include <rclcpp/logger.hpp>
 
 
 namespace helios_cv {
 
 class OVNetDetector : public BaseNetDetector {
 public:
+    OVNetDetector(const BaseNetDetectorParams& params);
+
+    ~OVNetDetector();
+
     ArmorsStamped detect_armors(const ImageStamped& image_stamped) final;
 
     void set_params(void* params) final;
 
     std::map<std::string, const cv::Mat*> get_debug_images() final;
-
-
 private:
+    void clear_queue(std::queue<std::future<ArmorsStamped>>& futs);
 
+    ArmorsStamped armors_stamped_;
+    ImageStamped image_stamped_;
 
+    int frames_;
+
+    Inference* infer1_;
+    Inference* infer2_;
+    Inference* infer3_;
+
+    std::vector<Inference*> detect_vector_;
+    std::queue<std::future<ArmorsStamped>> futs_;
+
+    BaseNetDetectorParams params_;
+
+    rclcpp::Logger logger_ = rclcpp::get_logger("OVNetDetector");
 };
 
 } // namespace helios_cv
--- net_detectors/src/NetDetectors.cpp
+++ net_detectors/src/NetDetectors.cpp
@@ -3,18 +3,57 @@
 
 namespace helios_cv {
 
+OVNetDetector::OVNetDetector(const BaseNetDetectorParams& params) {
+    params_ = params;
+}
+
+OVNetDetector::~OVNetDetector() {
+    // delete infer_;
+    clear_queue(futs_);
+    for (int i = 0; i < params_.net_params.POOL_NUM; i++) {
+        delete detect_vector_[i];
+    }
+}
+
 ArmorsStamped OVNetDetector::detect_armors(const ImageStamped& image_stamped) {
+    armors_stamped_.armors.clear();
+    image_stamped_ = image_stamped;
+    if (image_stamped_.image.empty()) {
+        return armors_stamped_;
+    }
+    if (futs_.size() < params_.net_params.POOL_NUM) {
+        detect_vector_[futs_.size()]->img_ = image_stamped_;
+        futs_.push(thread_pool_->submit(&Inference::detect, &(*detect_vector_[futs_.size()])));
+    } else {
+        futs_.front().wait();
+        armors_stamped_ = std::move(futs_.front().get());
+        futs_.pop();
+        // header.stamp = this->now();
+        // sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(header, "rgb8", detect_vector[frames % POOL_NUM]->img_).toImageMsg();
+        // armor_pub_->publish(*msg);
+        detect_vector_[frames_ % params_.net_params.POOL_NUM]->img_ = image_stamped_;
+        futs_.push(thread_pool_->submit(&Inference::detect, &(*detect_vector_[frames_++ % params_.net_params.POOL_NUM])));
+        return armors_stamped_;
+    }
+    ArmorsStamped empty_armors;
+    return empty_armors;
 
 }
 
 void OVNetDetector::set_params(void* params) {
-
+    params_ = *static_cast<BaseNetDetectorParams*>(params);
 }
 
 std::map<std::string, const cv::Mat*> OVNetDetector::get_debug_images() {
-
+    std::map<std::string, const cv::Mat*> debug_images;
+    debug_images.emplace("result_img", &image_stamped_.image);
+    return debug_images;
 }
 
+void OVNetDetector::clear_queue(std::queue<std::future<ArmorsStamped>>& futs) {
+    std::queue<std::future<ArmorsStamped>> empty;
+    std::swap(futs_, empty);
+}
 
 
 } // namespace helios_cv
